18efefa231c1dcbeec1071a1a4081a2c75bfd1e3
amirmhmd485 <amirmhmd378@gmail.com>
2026-05-03 15:50:31 +0300

    first project with api and the ai who made the api and i build the logic

---
+++ main.js
@@ -0,0 +1,27 @@
+// code
+const images = document.querySelector(".images");
+const input = document.querySelector("input");
+const btn  = document.querySelector("button");
+
+async function getApi(url){
+    let response = await fetch(url);
+    let data = await response.json();
+    return data;
+}
+btn.addEventListener("click" , function(e){
+    images.innerHTML = "";
+    getApi("images.json").then((data) => {
+        if(input.value != ""){
+            for(let i = 0 ; i < +input.value ; i++){
+                let img = document.createElement("img");
+                img.src = data[Math.floor(Math.random() * data.length)].src;
+                img.setAttribute("loading" , "lazy");
+                images.appendChild(img);
+            }
+            images.style.padding = "5px";
+        }
+        else{
+            e.preventDefault();
+        }
+    })
+});
